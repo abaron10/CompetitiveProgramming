da934a40f6e5347b671a0d233d79b11ba37f7842
andres.baron <andres.baron@rappi.com>
2023-05-23 19:43:48 -0500

    bitwise exercise added

---
+++ LeetCode/BitwiseOps/single_number.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+#include <vector>
+#include <bitset>
+
+using std::cout; 
+using std::vector;
+using std::bitset;
+
+
+int singleNumber(vector<int> nums)
+{
+    int ans = 0;
+    for (auto x : nums)
+    {
+        ans ^= x;
+        bitset<4> s(ans);
+        cout << s.to_string() << '\n';
+    }
+    return ans;
+}
+
+int main()
+{
+
+    cout << singleNumber({2,1, 2});
+    
+    return 0;
+}
